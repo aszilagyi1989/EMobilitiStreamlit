07c265d12e0b80c647c8c2b609e9c76f0c4196e3
vl05953 <adam.szilagyi@ksh.hu>
2026-07-08 10:49:25 +0200

    ttl = 1

--- Main.py
+++ Main.py
@@ -25,10 +25,10 @@ def load_data():
 
   return df
 
-@st.cache_data
+@st.cache_data(ttl = 1)
 def load_data2():
   df = fetch_raw_data().copy()
-  # df["Berendezés leszerelésének dátuma"] = pd.to_datetime(df["Berendezés leszerelésének dátuma"], format = "mixed", errors = "coerce")
+  df["Berendezés leszerelésének dátuma"] = pd.to_datetime(df["Berendezés leszerelésének dátuma"], format = "mixed", errors = "coerce")
   df["IRSZ_VAROS"] = (df["Töltőberendezés irányítószáma"].astype(str) + " " + df["Töltőberendezés település megnevezése"])
 
   return df
@@ -98,27 +98,27 @@ with st.sidebar:
     RAW_DATAS["Berendezés üzembehelyezésének dátuma"] = pd.to_datetime(RAW_DATAS["Berendezés üzembehelyezésének dátuma"], format = "mixed", errors = "coerce")
     RAW_DATAS["Berendezés leszerelésének dátuma"] = pd.to_datetime(RAW_DATAS["Berendezés leszerelésének dátuma"], format = "mixed", errors = "coerce")
     
-    # # 2. ÚJ JAVÍTÓ LOGIKA: Ha az évszám 222, vagy a dátum NaT lett a hiba miatt, 
-    # # de tudjuk, hogy ez a gép 2022-es (és a leszerelése üres, tehát még aktív):
-    # # Kikeressük azokat a sorokat, ahol a leszerelés hiányzik, de az üzembehelyezés gyanús (222-es év vagy NaT)
-    # gyanus_szurese = (
-    #     (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].dt.year == 222) | 
-    #     (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].isna())
-    # ) & (RAW_DATAS["Berendezés leszerelésének dátuma"].isna())
-    # 
-    # # Kényszerítjük rájuk a javított, pontos 2022-02-26-os dátumot
-    # RAW_DATAS.loc[gyanus_szurese, "Berendezés üzembehelyezésének dátuma"] = pd.Timestamp("2022-02-26")
-    # 
-    
-    # 2. Golyóálló javítás: Ha az évszám 222 (vagy NaT lett a formátumhiba miatt), 
-  # de a gép még üzemel (leszerelés hiányzik), akkor felülírjuk a helyes modern dátumra.
+    # 2. ÚJ JAVÍTÓ LOGIKA: Ha az évszám 222, vagy a dátum NaT lett a hiba miatt,
+    # de tudjuk, hogy ez a gép 2022-es (és a leszerelése üres, tehát még aktív):
+    # Kikeressük azokat a sorokat, ahol a leszerelés hiányzik, de az üzembehelyezés gyanús (222-es év vagy NaT)
     gyanus_szurese = (
-      (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].dt.year == 222) | 
-      (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].isna())
+        (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].dt.year == 222) |
+        (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].isna())
     ) & (RAW_DATAS["Berendezés leszerelésének dátuma"].isna())
-  
+
+    # Kényszerítjük rájuk a javított, pontos 2022-02-26-os dátumot
     RAW_DATAS.loc[gyanus_szurese, "Berendezés üzembehelyezésének dátuma"] = pd.Timestamp("2022-02-26")
-  
+
+    
+  #   # 2. Golyóálló javítás: Ha az évszám 222 (vagy NaT lett a formátumhiba miatt), 
+  # # de a gép még üzemel (leszerelés hiányzik), akkor felülírjuk a helyes modern dátumra.
+  #   gyanus_szurese = (
+  #     (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].dt.year == 222) | 
+  #     (RAW_DATAS["Berendezés üzembehelyezésének dátuma"].isna())
+  #   ) & (RAW_DATAS["Berendezés leszerelésének dátuma"].isna())
+  # 
+  #   RAW_DATAS.loc[gyanus_szurese, "Berendezés üzembehelyezésének dátuma"] = pd.Timestamp("2022-02-26")
+  # 
     
     # Kivágjuk az évszámot a szövegből (pl. "2025.12.31" -> 2025) és felépítjük a datetime objektumot
     year_int = int(selected_year.split(".")[0])
